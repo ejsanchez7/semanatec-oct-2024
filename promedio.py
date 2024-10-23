5251d0f52e30ee9200878bef4bad8b66be3ec502
Erik Sánchez <ejsanchez@tec.mx>
2024-10-22 19:16:58 -0600

    Agregar variable para la cantidad de numeros

--- promedio.py
+++ promedio.py
@@ -1,7 +1,8 @@
 suma = 0
+cantidad = 4
 
-for i in range(4):
+for i in range(cantidad):
 	num = float(input("Escribe un número: "))
 	suma += num
 
-print(f"El promedio es: {suma / 4}")
+print(f"El promedio es: {suma / cantidad}")
